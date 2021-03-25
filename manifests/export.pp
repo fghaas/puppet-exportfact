4c900ee81dc26e9a53549ee67fc78a578ed6b1b0
Udo Mueller <udo.mueller@plath.de>
2021-03-25 08:16:53 +0000

    added windows support

--- manifests/export.pp
+++ manifests/export.pp
@@ -3,21 +3,36 @@ define exportfact::export (
   $category = "fact",
 ) {
 
-  $categoryfile = "$exportfact::factsdir/$category.txt"
+  $categoryfile_win = "$exportfact::factsdir_win/$category.txt"
+  $categoryfile_lin = "$exportfact::factsdir_lin/$category.txt"
 
-  ensure_resource('file',
-                  "$categoryfile",
-                  { owner => "root",
-                    group => "root",
-                    mode => "0640" }) 
+  if $facts['os']['family'] == 'windows' {
+    file {"$categoryfile_win":
+      ensure => 'file',
+    }
+  } else {
+    ensure_resource('file',
+                    "$categoryfile_lin",
+                    { owner => "root",
+                      group => "root",
+                      mode => "0640" }) 
+  }
 
-  @@augeas { "fact_$name":
-    context => "/files$categoryfile",
-    incl => "$categoryfile",
+  @@augeas { "fact_${name}_lin":
+    context => "/files$categoryfile_lin",
+    incl => "$categoryfile_lin",
     lens => "Shellvars.lns",
     changes => "set $name $value",
-    require => [Class['exportfact'],File[$categoryfile]],
-    tag => "fact_$category"
+    require => [Class['exportfact'],File[$categoryfile_lin]],
+    tag => ["fact_$category","RedHat"],
+  }
+
+  @@file_line { "fact_${name}_win":
+    path => "$categoryfile_win",
+    line => "$name=$value",
+    match => "^$name=",
+    require => [Class['exportfact'],File[$categoryfile_win]],
+    tag => ["fact_$category","windows"],
   }
 
 }
